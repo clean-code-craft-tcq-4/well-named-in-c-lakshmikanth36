3367383f051c74a4f98767b6e6a1291d3bb306d2
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-15 21:59:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -37,10 +37,9 @@ void testPairToNumber( enum MajorColor major,enum MinorColor minor,int expectedP
 }
 
 int main() {
-    for (int i=1;i>=25;i++){
-    testNumberToPair(i, WHITE, BROWN); }
-    //testNumberToPair(5, WHITE, SLATE);
-    //testPairToNumber(BLACK, ORANGE, 12);
-    //testPairToNumber(VIOLET, SLATE, 25);
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
     return 0;
 }
