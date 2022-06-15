a7cec03b3ba1e6a5c06f06d83d7a710739f62176
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-15 22:23:59 +0530

    Update ColorPairTest.c

--- ColorPairTest.c
+++ ColorPairTest.c
@@ -1,3 +1,5 @@
+#include "header.h"
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);}
 
