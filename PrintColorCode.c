9fed3e4bfc92f96b0fa7fe7c5e35e5da8c43f383
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-16 21:36:00 +0530

    Update PrintColorCode.c

--- PrintColorCode.c
+++ PrintColorCode.c
@@ -5,12 +5,13 @@ void PrintColorCode()
     int MajorColor;
     int MinorColor;
     int PairNumber = 1;
-
+    
+    printf("  Pair No.  Major Color   Minor Color\n");
     for(MajorColor = 0; MajorColor < numberOfMajorColors; MajorColor++)
     {
         for(MinorColor = 0; MinorColor < numberOfMinorColors; MinorColor++)
         {
-            printf(" %d\t  %s\t  %s\t\n",PairNumber++, majorColors[MajorColor], minorColors[MinorColor]);
+            printf("   %d \t    %s\t  %s\t\n",PairNumber++, majorColors[MajorColor], minorColors[MinorColor]);
         }
       
     }
