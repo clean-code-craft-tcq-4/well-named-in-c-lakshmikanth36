adf6a3132a72ca8164985e7f09f63eb534c39520
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-16 21:18:15 +0530

    Update PrintColorCode.c

--- PrintColorCode.c
+++ PrintColorCode.c
@@ -4,11 +4,11 @@ void PrintColorCode()
     int MinorColor;
     int PairNumber = 1;
 
-    for(MajorColor = 0; MajorColor < numberOfMajorColors; majorColor++)
+    for(MajorColor = 0; MajorColor < numberOfMajorColors; MajorColor++)
     {
-        for(MinorColor = 0; MinorColor < numberOfMinorColors; ,MinorColor++)
+        for(MinorColor = 0; MinorColor < numberOfMinorColors; MinorColor++)
         {
-            printf("   %d\t  %s\t  %s\t\n",PairNumber++, MajorColors[MajorColor], MinorColors[MinorColor]);
+            printf(" %d\t  %s\t  %s\t\n",PairNumber++, MajorColors[MajorColor], MinorColors[MinorColor]);
         }
       
     }
