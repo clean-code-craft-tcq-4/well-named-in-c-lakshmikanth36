4c856d96da4248f7e30215c41ffbce0bbf846af2
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-16 21:05:55 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -11,8 +11,14 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+extern const char* MajorColors[];
+extern const char* MinorColors[];
+extern int numberOfMajorColors ;
+extern int numberOfMinorColors ;
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+int DisplayColorCode();
