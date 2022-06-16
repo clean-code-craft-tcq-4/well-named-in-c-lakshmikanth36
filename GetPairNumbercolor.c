cd20c461978a8c9e0127c3eec3178139962472b6
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-16 13:40:40 +0530

    Update GetPairNumbercolor.c

--- GetPairNumbercolor.c
+++ GetPairNumbercolor.c
@@ -2,9 +2,6 @@
 #include <assert.h>
 #include "header.h"
 
-extern const char* MinorColorNames[];
-extern int numberOfMinorColors;
-
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1; }
 
