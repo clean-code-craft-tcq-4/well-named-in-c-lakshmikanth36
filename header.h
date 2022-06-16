088efa16aa4c66ce40c8b2a543316b396ee97409
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-16 13:42:35 +0530

    Update header.h

--- header.h
+++ header.h
@@ -5,7 +5,8 @@ int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate"};
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
